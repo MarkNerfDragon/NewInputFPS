6e799bcf4c6ff735341bb27680067b76fa53ee48
MarkDevy <128891378+MarkNerfDragon@users.noreply.github.com>
2023-05-11 22:31:48 +0500

    Add files via upload

---
+++ Assets/Scripts/WallRunning.cs
@@ -0,0 +1,192 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRunning : MonoBehaviour
+{
+    private InputMaster controls;
+
+    [Header("Wallrunning")]
+    public LayerMask whatIsWall;
+    public LayerMask whatIsGround;
+    public float wallRunForce;
+    public float wallJumpUpForce;
+    public float wallJumpSideForce;
+
+    [Header("Input")]
+    public float horizontalInput;
+    public float verticalInput;
+
+    [Header("Exiting")]
+    public float exitWallTime;
+    private bool exitingWall;
+    private float exitWallTimer;
+
+    [Header("Gravity")]
+    public bool useGravity;
+    public float gravityCounterForce;
+
+    [Header("Detection")]
+    public float wallCheckDistance;
+    public float minJumpHeight;
+    private RaycastHit leftWallhit;
+    private RaycastHit rightWallhit;
+    private bool wallLeft;
+    private bool wallRight;
+
+    [Header("References")]
+    public Transform orientation;
+    private PlayerMovement pm;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    void Awake()
+    {
+        controls = new InputMaster();
+    }
+
+    void Update()
+    {
+        CheckForWall();
+        StateMachine();
+    }
+
+    void FixedUpdate()
+    {
+        if(pm.wallrunning)
+        {
+            WallRunningMovement();
+        }
+    }
+
+    private void CheckForWall()
+    {
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallhit, wallCheckDistance, whatIsWall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallhit, wallCheckDistance, whatIsWall);
+    }
+
+    private bool AboveGround()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
+    }
+
+    private void StateMachine()
+    {
+        //getting inputs
+        Vector2 input = controls.PlayerActions.Movement.ReadValue<Vector2>();
+
+        horizontalInput = input.x;
+        verticalInput = input.y;
+
+        //wallrunning
+        if((wallLeft || wallRight) && verticalInput > 0 && AboveGround() && !exitingWall)
+        {
+            if(!pm.wallrunning)
+            {
+                StartWallRun();
+            }
+
+            if(controls.PlayerActions.WallJump.triggered)
+            {
+                wallJump();    
+            }
+        }
+
+        else if(exitingWall)
+        {
+            if(pm.wallrunning)
+            {
+                StopWallRun();
+            }
+
+            if(exitWallTimer > 0)
+            {
+                exitWallTimer -= Time.deltaTime;
+            }
+
+            if(exitWallTimer <= 0)
+            {
+                exitingWall = false;
+            }
+        }
+
+        else
+        {
+            if(pm.wallrunning)
+            {
+                StopWallRun();
+            }
+        }
+    }
+
+    private void StartWallRun()
+    {
+        pm.wallrunning = true;
+
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+    }
+
+    private void WallRunningMovement()
+    {
+        rb.useGravity = useGravity;
+
+        Vector3 wallNormal = wallRight ? rightWallhit.normal : leftWallhit.normal;
+
+        Vector3 wallForward = Vector3.Cross(wallNormal, transform.up);
+
+        if((orientation.forward - wallForward).magnitude > (orientation.forward - -wallForward).magnitude)
+        {
+            wallForward = -wallForward;
+        }
+
+        //forward force
+        rb.AddForce(wallForward * wallRunForce, ForceMode.Force);
+
+        //push to wall force
+        if(!(wallLeft && horizontalInput > 0) && !(wallRight && horizontalInput < 0))
+        {
+            rb.AddForce(-wallNormal * 100, ForceMode.Force);
+        }
+
+        if(useGravity)
+        {
+            rb.AddForce(transform.up * gravityCounterForce, ForceMode.Force);
+        }
+    }
+
+    private void StopWallRun()
+    {
+        pm.wallrunning = false;
+        rb.useGravity = true;
+    }
+
+    private void wallJump()
+    {
+        //enter state
+        exitingWall = true;
+        exitWallTimer = exitWallTime;
+
+        Vector3 wallNormal = wallRight ? rightWallhit.normal : leftWallhit.normal;
+
+        Vector3 forceToApply = transform.up * wallJumpUpForce + wallNormal * wallJumpSideForce;
+
+        //reset y velocity and add force
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(forceToApply, ForceMode.Impulse);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
